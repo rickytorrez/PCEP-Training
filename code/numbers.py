37e7be1fb46054f29aeb5d8ef5f413057550c28b
EC2 Default User <ec2-user@ip-172-31-91-208.ec2.internal>
2022-06-23 02:19:24 +0000

    numbers, operators, strings and Unary/Bitwise

---
+++ code/numbers.py
@@ -0,0 +1,6 @@
+print(2+2) # integer
+
+print(2.0 + 2.0) # float => fractional
+
+print(4.5e9) # 4.5 * (10 ** 9) => scientific notation
+
---
+++ code/strings.py
@@ -0,0 +1,26 @@
+print('single quoted string')
+
+print("double quoted string")
+
+print('''
+this is a triple quoted
+multi-line string
+''')
+
+# operators
+print("pass" + "word")
+
+laugh = "Ha" * 4 # * knows how to handle different types
+print(laugh)
+
+string = "My_StRiNg"
+print(string.find("t"))
+print(string.lower())
+
+print("PaSsWoRd1234".lower())
+
+print("Tab\tDelimited")
+print("New\nLine")
+
+print("'Single quotes in double'")
+print("\"Double\" in Double")
