f70312b84f053abf651178ecd7a2e7391706839e
rickytorrez <torrez.e.ricardo@gmail.com>
2023-01-31 02:21:19 +0000

    boiler

---
+++ training/hello.py
@@ -0,0 +1,10 @@
+
+# This is a full line comment
+# No block comments available 
+
+print("hello world!") # this is a trailing comment
+
+"""
+This is not a block comment
+This is a multi line string
+"""
---
+++ training/variables.py
@@ -0,0 +1,14 @@
+my_str = "This is a simple string"
+
+print(my_str)
+
+my_str = "This is a different string"
+
+# my_str = my_str + testing
+my_str += " testing"
+
+print(my_str)
+
+my_str = 1
+
+print(my_str)
