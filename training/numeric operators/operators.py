f47caa5bbcb768d7716c9fa56a62d22e7b774c71
rickytorrez <torrez.e.ricardo@gmail.com>
2023-02-02 01:36:37 +0000

    numeric operators

---
+++ training/numeric operators/operators.py
@@ -0,0 +1,20 @@
+# integers
+print(2+2)
+
+# floats
+print(2.0 + 2.0)
+
+# multiplication
+print(3*5)
+
+# division
+print(6/2)
+
+# floor division - how many times it evenly divides
+print( 5 // 3)
+
+# modulo - remainder, useful to determine if something is even or odd
+print (5 % 3)
+
+# exponents
+print(2 ** 3)
